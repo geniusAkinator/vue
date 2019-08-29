5f6629c77f190212c74294e7c6ab4477f58cbcdf
wcg <wcg142857@gmail.com>
2019-08-29 09:36:12 +0800

    111111

--- src/views/realtime/detail.vue
+++ src/views/realtime/detail.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
     <el-row :gutter="20">
-<<<<<<< HEAD
       <el-col :span="14">
         <my-card>
           <template slot="title">
@@ -30,22 +29,10 @@
           </template>
           <template slot="content">ddddddddd</template>
         </my-card>
-=======
-      <el-col :span="16">
-        <div class="">
-            <div></div>
-        </div>
-      </el-col>
-      <el-col :span="8">
-        <div>
-            dddddddd
-        </div>
->>>>>>> 878008ff909f2e83da98a108afc505fba2b37e60
       </el-col>
     </el-row>
   </div>
 </template>
-<<<<<<< HEAD
        
 
 <script>
@@ -56,23 +43,12 @@ export default {
   },
   components: {
     MyCard
-=======
-
-<script>
-export default {
-  data() {
-    return {};
->>>>>>> 878008ff909f2e83da98a108afc505fba2b37e60
   }
 };
 </script>
 
-<<<<<<< HEAD
 <style scoped>
 .el-row+.el-row{
   margin-top:20px 
 }
-=======
-<style>
->>>>>>> 878008ff909f2e83da98a108afc505fba2b37e60
 </style>
